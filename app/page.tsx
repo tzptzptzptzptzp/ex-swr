3f586abbc3325b4f3e63f7c6784a48d181534d06
tzptzptzptzptzp <git@tzp.pink>
2023-12-18 23:51:18 +0900

    ⚡️ データ取得中はローディングを表示するように変更

--- app/page.tsx
+++ app/page.tsx
@@ -16,14 +16,18 @@ const API_URL = "https://jsonplaceholder.typicode.com/users/1";
 
 export default function Home() {
   const { data, error, isLoading } = useSWR(API_URL, fetcher);
-  console.log(data);
-
   return (
     <main className="flex items-center justify-center w-screen h-screen bg-amber-200">
       <div className="flex flex-col gap-2 p-20 rounded-3xl bg-white shadow-md text-center">
         <h2 className="text-3xl">- UserProfile -</h2>
-        <p className="text-2xl">name: {data?.name}</p>
-        <p>email: {data?.email}</p>
+        {isLoading ? (
+          <p>Loading...</p>
+        ) : (
+          <>
+            <p className="text-2xl">name: {data?.name}</p>
+            <p>email: {data?.email}</p>
+          </>
+        )}
       </div>
     </main>
   );
