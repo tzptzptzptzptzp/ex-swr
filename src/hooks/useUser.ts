138b9a05a7c70d89405463c3907a02982f58a22e
tzptzptzptzptzp <git@tzp.pink>
2023-12-19 00:04:53 +0900

    ⚡️ idを受け取り、該当するデータを取得するように変更

--- src/hooks/useUser.ts
+++ src/hooks/useUser.ts
@@ -10,10 +10,10 @@ async function fetcher(key: string) {
   return fetch(key).then((res) => res.json() as Promise<User | null>);
 }
 
-const API_URL = "https://jsonplaceholder.typicode.com/users/1";
+const API_URL = "https://jsonplaceholder.typicode.com/users/";
 
-export const useUser = () => {
-  const { data, error, isLoading } = useSWR(API_URL, fetcher);
+export const useUser = (id: number) => {
+  const { data, error, isLoading } = useSWR(`${API_URL}${id}`, fetcher);
 
   return {
     user: data,
