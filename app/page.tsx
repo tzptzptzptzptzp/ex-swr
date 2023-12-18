b27d2eb9eb44b153129a0f2ffe590946446128e8
tzptzptzptzptzp <git@tzp.pink>
2023-12-18 23:45:29 +0900

    📝 APIパスを追加

--- app/page.tsx
+++ app/page.tsx
@@ -8,6 +8,8 @@ async function fetcher(key: string) {
   return fetch(key).then((res) => res.json() as Promise<User | null>);
 }
 
+const API_URL = "https://jsonplaceholder.typicode.com/users/1";
+
 export default function Home() {
   return <main>SWR</main>;
 }
